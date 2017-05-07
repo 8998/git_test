b0853a3bdd70166eb38a3a11feac2d7c4f8b0061
= <=>
2017-05-07 11:32:03 +0800

    test git

--- callback.js
+++ callback.js
@@ -1,9 +1,8 @@
 /**
  * Created by Ryso on 2015/12/1.
  */
-// 回调函数实例
+
 'use strict';
 var foo = function(callback){
     (callback && typeof (callback) === 'function') && callback();
-    debugger;
 };
